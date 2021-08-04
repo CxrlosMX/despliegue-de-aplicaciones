0656cb9b84b451dea775a60b2e800cfa9fba4bd5
CxrlosMX <LuisCRendon131@gmail.com>
2021-08-03 19:13:45 -0500

    Uso de Applets I

---
+++ src/mis_applets/HolaAlumnos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mis_applets;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author: CxrlosMX
+ * @Git-Hub: https://github.com/CxrlosMX
+ * @Phone: 953-212-97-27
+ * @Email: LuisCRendon131@gmail.com
+ * @Date: 2/08/2021
+ *
+ */
+public class HolaAlumnos extends JApplet { //Heredamos de la clase JApplet
+
+//Un Applet no lleva el método main
+    @Override
+    public void init() {
+        //Creamos el mensaje que queremos que aparezca
+        JLabel rotulo=new JLabel("Hola Alumnos");
+        
+        //Agregamos nuestro mensaje a nuestro applet
+        add(rotulo);
+        //Esto se ejecutara en nuestro navegador
+        
+    }
+}
